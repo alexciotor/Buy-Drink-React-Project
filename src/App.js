29319ee527b29bf72f29c0ea88dce618b7b5f4bd
alexciotor <ciotor.alex@yahoo.com>
2021-02-19 02:38:00 +0200

    drink object refractor

--- src/App.js
+++ src/App.js
@@ -10,53 +10,60 @@ import Nav from './components/nav'
 import SingleDrink from './components/singleDrink'
  import {useRef} from 'react'
  import drinks from './modules/drinks'
-const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
 const initialInfo = {
 loading:false,
-items:drinks,
+items:[],
 amount:1,
+price: 0
+ 
 }
 
-// const getPrice= (id) =>{
-//     let price = Number(id) /1000;
-//     return price.toFixed(2) 
+
 const reducer =  (state,action)=>{
     if(action.type=== 'DATA'){
         return {...state, items:action.payload}
     }
 }
 function App() {
+    const [link,setLink] = useState('a')
+    const [total,setTotal ] = useState([])
 const[display, setDisplay] =useState(false)
 const [state, dispatch] = useReducer(reducer, initialInfo)
  const [cart, setCart] = useState([])
- 
-const [cartItems, setCartItems] = useState([])
+ const searchValue = React.useRef('')
+console.log(link);
  const getData = async()=>{
-     const response = await  fetch(URL)
+     const response = await  fetch(`${URL}${link}`)
      const data = await response.json()
-    
-     
+     const drinks = data.drinks
+    dispatch({type:'DATA',payload:drinks})     
  }
+
 const getCart = (id)=>{
       state.items.filter(item=>{
          if(item.idDrink ===id){
             setCart([...cart,item])
          }
-          
       return item
     })
+}
+const getPrice= (id) =>{
+    let price = Number(id) /1000;
+  return  price.toFixed(2) 
+ 
+
  
 }
- console.log(cart);
- 
  useEffect(()=>{
-     getData()
+ getData()
  },[])
+ 
 return  <Router>   
 <Nav cart={cart}/>
 <Switch>
 <Route exact path='/'>
-<Home  setCart={setCart} setDisplay={setDisplay} getCart={getCart}   state = {state} />
+<Home setLink={setLink} searchValue={searchValue} setTotal = {setTotal} total= {total}  setCart={setCart} setDisplay={setDisplay} getCart={getCart} getPrice={getPrice}  state = {state} />
 </Route>
 <Route path='/about'>
 <About/>
@@ -66,7 +73,7 @@ return  <Router>
 </Route>
 <Route path='/cart'>
    
-      <Cart   display={display} cart ={cart}/>
+      <Cart total= {total} getPrice={getPrice}  display={display} cart ={cart}/>
  
 
 </Route>
