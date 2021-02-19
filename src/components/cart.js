4941a2d6adde0b415ca1fe104b8bb967883700eb
alexciotor <ciotor.alex@yahoo.com>
2021-02-19 02:38:54 +0200

    total added

--- src/components/cart.js
+++ src/components/cart.js
@@ -4,23 +4,27 @@ import { CgLogOff } from 'react-icons/cg'
 import { FaRProject } from 'react-icons/fa'
  import Empty from '../components/empty'
    
-const Cart = ({display,cart  })=>{
+const Cart = ({display,cart ,getPrice, total  })=>{
   const [newCart, setNewCart] = useState([...new Set(cart)])
  
-let sum = newCart.map(item=>{
-  return item.price
-})
  
-
-   let lastTotal = sum.reduce((total,item)=>total+item,0)
-
+// let sum = newCart.map(item=>{
+//   return item.price
+// })
+ 
+let  lastTotal = [... new Set(total)]
+let totalAmount =  lastTotal.reduce((total,item)=>total+item,0)
+ 
+ 
   return (  
    <section className='section-cart' >
 
   <div className='cart-container' >
 {newCart.map(item=>{
   const {strDrink:name,idDrink:id,strCategory:cat,strAlcoholic:type, strGlass:glass, strInstructions:instruct,strDrinkThumb:img,strIngredient1:ingr1,strIngredient2:ingr2,strIngredient3:ingr3,strIngredient4:ingr4,price}=item
- 
+  let prices = getPrice(id)
+
+
   return (
     <div className="article-content">
 <article   className='cart-article' >
@@ -32,7 +36,7 @@ let sum = newCart.map(item=>{
 <h4>{name}</h4>
 <p>{glass}</p>
  <p>{type}</p>
-<p>{price} $</p>
+<p>{prices} $</p>
  
 </div>
 
@@ -50,19 +54,20 @@ let sum = newCart.map(item=>{
 </div>
    
 <div className="total-container">
-<h4>{display ? `total : ${lastTotal}  $` : <Empty/> } </h4>
+<h4>{display ? `total : ${totalAmount.toFixed(2)}  $` : <Empty/> } </h4>
 {display && <button className="cart-bt">Submit Order</button>}
 </div> 
+
 </section>
  
 
-
+ 
  
  
  
 )
 
-   
+ 
 }
 
 export default Cart
